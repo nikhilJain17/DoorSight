06458e0e23c6af7beaeea7dd1d25f3bde7ca8f83
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-12-05 19:08:30 -0500

    move fast and break things

---
+++ fisherfaces.py
@@ -0,0 +1,7 @@
+import numpy as numpynav
+import cv2
+import os
+import math
+import glob
+#include <string>
+import string
--- lbph.py
+++ lbph.py
@@ -1,11 +1,12 @@
 import numpy as numpynav
 import cv2
+import os
 import math
 import glob
 #include <string>
 import string
 
-labels = []
+# labels = []
 # images = []
 folders = glob.glob('train_faces/*')
 
@@ -15,21 +16,25 @@ def ID_from_filename(filename):
 
 
 def get_images_and_labels():
-	global images
+	global images, labels, nbr
 	images = []
+	labels = []
+	nbr = -69
 	for x, folder in enumerate(folders):
 		temp_images = glob.glob(folder + '/*')
 		images = temp_images + images
 		for i, face in enumerate(temp_images):
-			labels.append(ID_from_filename(face))
-
+			nbr = os.path.split(face)[1].split(".")[0]
+	        labels.append(nbr)
+			# labels.append(ID_from_filename(face))
+	# redo labels to be a num array
+	# like 1 2 3 etc
 
 get_images_and_labels()
 
-print images
 
 #lbph 
-# recognizer = cv2.createLBPHFaceRecognizer()
+recognizer = cv2.createLBPHFaceRecognizer()
 
 # Perform the tranining
-# recognizer.train(images, np.array(labels))
+recognizer.train(images, numpynav.array(labels))
