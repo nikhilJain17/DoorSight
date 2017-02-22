091b91a125f9526826ef71eb349f5ef42f93873f
nikhilJain17 <nikhil.jain0987@gmail.com>
2017-02-21 20:17:15 -0500

    flask server interfaced with form from login.html, can render new html page and send data

--- Website_Code/app/server.py
+++ Website_Code/app/server.py
@@ -1,12 +1,20 @@
 # flask app
 # whats up doc
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
+def welcome():
 	print 'Hello!'
+	# serve up the login page
 	return render_template('login.html')
+
+
+@app.route('/attempt_login', methods=['POST'])
+def attempt_login():
+	print 'test'
+	print request.args
+	return render_template('selectMode.html')
