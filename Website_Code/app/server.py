6c6e01d2c428dc298f358aa078b940acff10ec46
nikhilJain17 <nikhil.jain0987@gmail.com>
2017-02-27 17:53:21 -0500

    working on socketio client side for raspi

--- Website_Code/app/server.py
+++ Website_Code/app/server.py
@@ -1,11 +1,15 @@
 # flask app
-# whats up doc
-
+# dependencies: flask, flask-socketio
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+
+# init the libraries
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
+socketio = SocketIO(app)
+ 
+# pass the routes and route handlers to the flask app
 @app.route('/')
 def welcome():
 	print 'Hello!'
@@ -16,5 +20,17 @@ def welcome():
 @app.route('/attempt_login', methods=['POST'])
 def attempt_login():
 	print 'test'
-	print request.args
+	# hash or something later?
+	username = str(request.form.getlist('username')[0])
+	password = str(request.form.getlist('password')[0])
+
+	# for testing only, delete later
+	print "U: " + username + '\nP: ' + password 
 	return render_template('selectMode.html')
+
+# pass the events and event handlers to the websocket
+@socketio.on('connect', namespace='/')
+def connected():
+	print 'somebody connected!'
+
+	# make sure the raspi connects with sockets to this server
